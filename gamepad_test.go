29912fedcd1b14799ddcff6e16b5bc9f3429acd9
Gintautas Kazlauskas <gintautaskazlauskas@protonmail.com>
2022-07-19 05:04:12 +0300

    Fixed gamepad unit test

--- gamepad_test.go
+++ gamepad_test.go
@@ -2,25 +2,23 @@ package uinput
 
 import (
 	"testing"
-	"time"
-	"uinput"
 )
 
 // This test inputs the konami code
 func TestInfiniteKonami(t *testing.T) {
-	vg, err := uinput.CreateGamepad("/dev/uinput", []byte("Hot gophers in your area"), 0xDEAD, 0xBEEF)
+	vg, err := CreateGamepad("/dev/uinput", []byte("Hot gophers in your area"), 0xDEAD, 0xBEEF)
 	if err != nil {
 		t.Fatalf("Failed to create the virtual gamepad. Last error was: %s\n", err)
 	}
 
 	for i := 0; i < 10; i++ {
 		for j := 0; j < 3; j++ {
-			err = vg.ButtonPress(uinput.ButtonDpadUp)
+			err = vg.ButtonPress(ButtonDpadUp)
 			if err != nil {
 				t.Fatalf("Failed to send button press. Last error was: %s\n", err)
 			}
 
-			err = vg.ButtonPress(uinput.ButtonDpadDown)
+			err = vg.ButtonPress(ButtonDpadDown)
 			if err != nil {
 				t.Fatalf("Failed to send button press. Last error was: %s\n", err)
 			}
@@ -28,29 +26,29 @@ func TestInfiniteKonami(t *testing.T) {
 		}
 
 		for j := 0; j < 3; j++ {
-			err = vg.ButtonPress(uinput.ButtonDpadLeft)
+			err = vg.ButtonPress(ButtonDpadLeft)
 			if err != nil {
 				t.Fatalf("Failed to send button press. Last error was: %s\n", err)
 			}
 
-			err = vg.ButtonPress(uinput.ButtonDpadRight)
+			err = vg.ButtonPress(ButtonDpadRight)
 			if err != nil {
 				t.Fatalf("Failed to send button press. Last error was: %s\n", err)
 			}
 
 		}
 
-		err = vg.ButtonPress(uinput.ButtonSouth)
+		err = vg.ButtonPress(ButtonSouth)
 		if err != nil {
 			t.Fatalf("Failed to send button press. Last error was: %s\n", err)
 		}
 
-		err = vg.ButtonPress(uinput.ButtonEast)
+		err = vg.ButtonPress(ButtonEast)
 		if err != nil {
 			t.Fatalf("Failed to send button press. Last error was: %s\n", err)
 		}
 
-		err = vg.ButtonPress(uinput.ButtonStart)
+		err = vg.ButtonPress(ButtonStart)
 		if err != nil {
 			t.Fatalf("Failed to send button press. Last error was: %s\n", err)
 		}
@@ -59,7 +57,7 @@ func TestInfiniteKonami(t *testing.T) {
 
 // This test moves the axes around a bit
 func TestAxisMovement(t *testing.T) {
-	vg, err := uinput.CreateGamepad("/dev/uinput", []byte("Hot gophers in your area"), 0xDEAD, 0xBEEF)
+	vg, err := CreateGamepad("/dev/uinput", []byte("Hot gophers in your area"), 0xDEAD, 0xBEEF)
 	if err != nil {
 		t.Fatalf("Failed to create the virtual gamepad. Last error was: %s\n", err)
 	}
